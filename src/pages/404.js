2d7da0a1188d3b8f09f9efd6477f0bf2539246e4
Michael Plazek <michael.plazek91@gmail.com>
2020-06-13 17:26:32 -0600

    Remove bad import

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,9 @@
 import React from "react";
 
-import Seo from "../components/seo";
 import App from "../app/App";
 
 const NotFoundPage = () => (
   <App>
-    <Seo title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </App>
