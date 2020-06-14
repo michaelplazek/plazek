97ead21fe2e1cdac2cbeb49bcf10b34ab2a51120
Michael Plazek <michael.plazek91@gmail.com>
2020-06-14 14:41:38 -0600

    Fix buildtime issue

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -1,9 +1,9 @@
 import Bowser from "bowser";
 
-const browser = Bowser.getParser(window.navigator.userAgent);
+const browser = () => Bowser.getParser(window.navigator.userAgent);
 
-export const getBrowser = () => browser.getBrowser();
-export const needsFooterSupport = () => browser.satisfies({
+export const getBrowser = () => browser().getBrowser();
+export const needsFooterSupport = () => browser().satisfies({
   mobile: {
     safari: '>=9',
     chrome: '>=80'
