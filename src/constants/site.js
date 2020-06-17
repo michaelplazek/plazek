b29fad9239a06356411c03b63d9b640a8f5347cd
Michael Plazek <michael.plazek91@gmail.com>
2020-06-16 20:50:02 -0600

    Update application text

--- src/constants/site.js
+++ src/constants/site.js
@@ -17,7 +17,7 @@ export default [
     description: 'This application was built with React and Grommet. I\'ve grown to love React. It does an excellent job ' +
       'of abstracting the DOM, allows you to structure your code base by function instead of language with JSX, and employs a ' +
       'unidirectional data flow which makes managing your data lifecycle a breeze (usually). For the component library, I used Grommet. ' +
-      'Grommet makes building flexible UIs easy, in that it is unopinionated and responsive.',
+      'Grommet is unopinionated and responsive; it makes building flexible UIs easy. It also has an awesome community behind it.',
     image: ApplicationGraphic,
   },
   {
@@ -25,16 +25,15 @@ export default [
     description: 'Gatsby is categorized as a static site builder, but it is so much more. It completely abstracts away the bundler and ' +
       'transpilation process, so no need to muck with Webpack and Babel. It also provides you with a local development environment, does ' +
       'bundle splitting automatically, allows you to prefetch and statically compile data at build time, uses GraphQL for everything (including ' +
-      'querying your filesystem), and has a full suite of plugins for various purposes. In my experience, it\'s unmatched in terms of ' +
-      'providing a rich and usable developer experience when building RIAs.',
+      'querying your filesystem), and has a full suite of plugins for various purposes. It\'s easily my new favourite framework.',
     image: BuildGraphic,
   },
   {
     title: 'Deploy',
-    description: 'After any code is pushed to Github and merged to master, Netlify triggers a build and deploy through managed CI/CD pipeline. ' +
+    description: 'After any code is pushed to Github and merged to master, Netlify triggers a build and deploy through a managed CI/CD pipeline. ' +
       'Maybe before I would have used CircleCI or Jenkins for my CI/CD, which would test, build, and push a bundle to an S3 bucket that was served ' +
-      'through CloudFront and connected to DNS via Route53. Now, all of that is abstracted by Netlify. They handle the entire DevOps stack for ' +
-      'you - SSL certs and all! They also have support for Lambdas and other serverless functionality that can tied in through various triggers.',
+      'through CloudFront and connected to DNS via Route53. Now, all of that is abstracted by Netlify. They handle the entire dev ops stack for ' +
+      'you - SSL certs and all! They also have support for Lambdas and other serverless functionality that can be tied in through various triggers.',
     image: DeployGraphic,
   },
 ];
