408ced01264dab1b2439b5245b9632624677c50b
Michael Plazek <michael.plazek91@gmail.com>
2020-06-17 16:44:07 -0600

    Add formatting and back button to blog post template

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,20 +1,55 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React, { useContext } from "react"
+import { graphql, Link } from "gatsby"
 import { withApp } from "../HOCs";
 import PageContainer from "../components/PageContainer";
-import { Box, Heading } from "grommet";
+import { Box, Heading, ResponsiveContext, Text } from "grommet";
+import { Previous as PreviousIcon } from 'grommet-icons';
 
 const Template = ({
   data,
 }) => {
   const { markdownRemark } = data;
   const { frontmatter, html } = markdownRemark;
-  console.log(frontmatter);
+
+  const size = useContext(ResponsiveContext);
+
   return (
     <PageContainer title={frontmatter.title}>
-      <Box className="blog-post">
-        <Heading margin='none' level={1}>{frontmatter.title}</Heading>
-        <Heading margin='none' level={2} size='small'>{frontmatter.subtitle}</Heading>
+      <Box>
+        <Box
+          direction='row'
+          align='center'
+          gap='small'
+          margin={{ bottom: 'medium' }}
+        >
+          <PreviousIcon
+            color='black'
+            size='small'
+          />
+          <Text>
+            <Link
+              to='/blog'
+              style={{
+                textDecoration: 'none',
+                color: 'black',
+              }}
+            >
+              Back
+            </Link>
+          </Text>
+        </Box>
+        <Box
+          direction={size !== 'small' ? 'row' : 'column'}
+          justify='between'
+          align={size !== 'small' ? 'center' : 'start'}>
+          <Box>
+            <Heading margin='none' level={1}>{frontmatter.title}</Heading>
+            <Heading margin={{ vertical: 'xsmall' }} level={2} size='small'>{frontmatter.subtitle}</Heading>
+          </Box>
+          <Box>
+            <Text>{frontmatter.date}</Text>
+          </Box>
+        </Box>
         <Box
           dangerouslySetInnerHTML={{ __html: html }}
         />
